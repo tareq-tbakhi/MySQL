6301691c648c5e91228966bc51d9bbddb50474dd
Tareq Tbakhi <tareq_tbakhi@hotmail.com>
2024-01-12 22:28:33 +0200

    NESTED QUERIES

--- company_database.sql
+++ company_database.sql
@@ -1,9 +1,11 @@
-SELECT employee.first_name, employee.last_name
-FROM employee
-WHERE employee.emp_id IN(
-  SELECT works_with.emp_id
-  FROM works_with
-  WHERE works_with.total_sales > 30000
+SELECT client.client_name
+FROM client
+WHERE client.branch_id = (
+  SELECT branch.branch_id
+  FROM branch
+  WHERE branch.mgr_id = 102
+  LIMIT 1
 );
+
 
 
